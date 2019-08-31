4e37b33081f986dd88e08f679692a0008e1f3158
Liam <saghendev@gmail.com>
2019-08-31 15:18:22 -0400

    I'm an idiot part 2

--- server/src/lib/seed-db.js
+++ server/src/lib/seed-db.js
@@ -3,7 +3,7 @@ const config = require('Config');
 
 module.exports = async () => {
   const { username, password } = config.get('auth');
-  if (await User.find({ username }).exec()) return;
+  if (await User.findOne({ username }).exec()) return;
 
   const user = new User({ username, password });
   await user.save();
