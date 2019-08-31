50d25abc51a6152472eff43fbbf1ebf2c9a345e2
Liam Dyer <saghendev@gmail.com>
2019-08-31 16:38:54 -0400

    Return token

--- server/src/routes/api/auth-api.js
+++ server/src/routes/api/auth-api.js
@@ -20,7 +20,7 @@ router.post('/login', async ctx => {
     maxAge: 1000 * 60 * 60 * 24 * 30 // 30 days
   });
 
-  ctx.ok({ message: 'Successfully logged in' });
+  ctx.ok({ message: 'Successfully logged in', token });
 });
 
 router.post('/logout', async ctx => {
